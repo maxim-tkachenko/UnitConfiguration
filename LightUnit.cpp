1d78b378673ca0aea2eb90b8350f728179c266d6
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-07-08 23:34:17 +0300

    cleanup lightunit class from arduino dependency

--- LightUnit.cpp
+++ LightUnit.cpp
@@ -1,5 +1,4 @@
 #include "LightUnit.h"
-#include "Arduino.h"
 
 LightUnit::LightUnit(Button *button, ILight *light)
 {
@@ -13,14 +12,8 @@ LightUnit::~LightUnit()
 
 void LightUnit::check()
 {
-    //Serial.println("LightUnit::check s");
     if (_button->readState())
     {
-        Serial.println("LightUnit::check true");
-        Serial.println(_light->get());
-        // Serial.println(__func__);
-        // Serial.println(__FUNCTION__);
-        // Serial.println(__PRETTY_FUNCTION__);
         _light->switchState();
     }
 }
