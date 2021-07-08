759fc8d7158b347beae9fbabc0c1382b74776ec7
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-07-08 23:48:10 +0300

    introduce runner

---
+++ Runner.cpp
@@ -0,0 +1,50 @@
+#include "FastLED.h"
+#include "Light.h"
+#include "FastLedLight.h"
+#include "ButtonDebounced.h"
+#include "LightUnit.h"
+
+#ifndef Runner_cpp
+#define Runner_cpp
+
+class Runner
+{
+#define NUM_LEDS 5 //71
+#define LED1_PIN 6
+
+#define NUM_LEDS2 143
+#define LED2_PIN 8
+
+private:
+    // Light lights1(2, 5, false);
+    // Light lights2(3, 7, false);
+    LightUnit *lightUnit3;
+    // FastLedLight lights4(10, false, NUM_LEDS, NUM_LEDS2);
+
+public:
+    ~Runner()
+    {
+        delete lightUnit3;
+    }
+
+    void setup()
+    {
+        lightUnit3 = new LightUnit(new ButtonDebounced(2 /*9*/),
+                                   FastLedLight::create<LED1_PIN>(false, NUM_LEDS));
+
+        // lights1.init();
+        // lights2.init();
+        // lights4.init<LED2_PIN>();
+    }
+
+    void loop()
+    {
+        lightUnit3->check();
+
+        // readButton3(lights1);
+        // readButton3(lights2);
+        // readButton3(lights4);
+    }
+};
+
+#endif
--- wall_switch_kitchen.ino
+++ wall_switch_kitchen.ino
@@ -1,20 +1,8 @@
-#include "FastLED.h"
+#include "Arduino.h"
+#include "Runner.cpp"
 // #include "ArduinoTrace.h"
-#include "Light.h"
-#include "FastLedLight.h"
-#include "ButtonDebounced.h"
-#include "LightUnit.h"
 
-#define NUM_LEDS 5 //71
-#define LED1_PIN 6
-
-#define NUM_LEDS2 143
-#define LED2_PIN 8
-
-// Light lights1(2, 5, false);
-// Light lights2(3, 7, false);
-LightUnit *lightUnit3;
-// FastLedLight lights4(10, false, NUM_LEDS, NUM_LEDS2);
+Runner runner;
 
 void setup()
 {
@@ -22,24 +10,15 @@ void setup()
   while (!Serial)
     ;
 
-  Serial.println("started3");
+  Serial.println("started4");
   // Serial.println(__func__);
   // Serial.println(__FUNCTION__);
   // Serial.println(__PRETTY_FUNCTION__);
 
-  lightUnit3 = new LightUnit(new ButtonDebounced(2 /*9*/),
-                             FastLedLight::create<LED1_PIN>(false, NUM_LEDS));
-
-  // lights1.init();
-  // lights2.init();
-  // lights4.init<LED2_PIN>();
+  runner.setup();
 }
 
 void loop()
 {
-  lightUnit3->check();
-
-  // readButton3(lights1);
-  // readButton3(lights2);
-  // readButton3(lights4);
+  runner.loop();
 }
