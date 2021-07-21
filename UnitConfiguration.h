d1870051d7a410e7f573049bf789b204f0b79550
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-07-21 23:29:46 +0300

    add lib header

---
+++ UnitConfiguration.h
@@ -0,0 +1,16 @@
+#ifndef UnitConfiguration_h
+#define UnitConfiguration_h
+
+#include "AVRPlatform.h"
+#include "ILight.h"
+#include "Button.h"
+#include "LightUnit.h"
+#include "IConfiguration.h"
+#include "Runner.h"
+
+// to be removed from lib
+#include "Light.h"
+#include "FastLedLight.h"
+#include "ButtonDebounced.h"
+
+#endif
