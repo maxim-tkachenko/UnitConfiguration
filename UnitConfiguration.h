1e3ac3bb282269e4b7e8c6fdf9a1688b5aa1e57c
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-08-16 23:49:00 +0300

    rearrange #includes

--- UnitConfiguration.h
+++ UnitConfiguration.h
@@ -3,14 +3,14 @@
 
 #include "AVRPlatform.h"
 #include "IDevice.h"
+#include "GenericDevice.h"
 #include "Button.h"
+#include "ButtonDebounced.h"
 #include "WorkUnit.h"
 #include "IConfiguration.h"
 #include "Runner.h"
 
 // to be removed from lib
-#include "GenericDevice.h"
 #include "FastLedLight.h"
-#include "ButtonDebounced.h"
 
 #endif
