6cd52523cfd18b5c8261aac8e923e826646e8598
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-08-31 00:58:14 +0300

    add comment

--- WorkUnit.cpp
+++ WorkUnit.cpp
@@ -58,6 +58,7 @@ WorkUnit::WorkUnit(WORKUNIT_ARGS, IHandler *handler)
     traceme;
 }
 
+// this could throw an exception when freeing already freed pointer.
 WorkUnit::~WorkUnit()
 {
     traceme;
