d79073dd0c4a3da5e62393f3af3dfbb753d508cd
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-09-28 22:34:18 +0300

    use debug config by default

--- example/example.ino
+++ example/example.ino
@@ -1,11 +1,12 @@
 #include "TestConfiguration.h"
+#include "DebugConfiguration.h"
 
 Runner runner;
 
 void setup()
 {
   runner.init();
-  runner.setConfig(new TestConfiguration());
+  runner.setConfig(new DebugConfiguration());
 }
 
 void loop()
