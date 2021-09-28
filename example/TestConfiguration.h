d1a420a6770bc6c6891e9bbc52817b4e68d0d2ad
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-09-28 20:58:58 +0300

    update example according to readme

--- example/TestConfiguration.h
+++ example/TestConfiguration.h
@@ -2,18 +2,35 @@
 #define TestConfiguration_h
 
 #include <UnitConfiguration.h>
+#include <TimeoutHandler.h>
 
-#define BASIC_CONFIG
-
-class TestConfiguration : public IConfiguration<1>
+class TestConfiguration : public IConfiguration<3>
 {
 public:
     TestConfiguration()
     {
-#ifdef BASIC_CONFIG
         // Setup primitive devices which can be controlled via relay and button, just specify data pins:
-        add(new WorkUnit(new GenericDevice(2), new PushButtonDebounced(3)));
-#endif
+        add(new WorkUnit(new GenericDevice(2), new PushButtonDebounced(12)));
+
+        // Setup leds using FastLED:
+        add(new WorkUnit(FastLedLight::create<3>(200), new PushButtonDebounced(11)));
+
+        // Setup two light zones which are controlled by two buttons and turned off after 15 mins of inactivity:
+        add(
+            new WorkUnit(
+                new IDevice *[2]
+                {
+                    new GenericDevice(4),
+                        new GenericDevice(5),
+                },
+                2,
+                new IController *[2]
+                {
+                    new PushButtonDebounced(10),
+                        new PushButtonDebounced(9),
+                },
+                2,
+                new TimeoutHandler(1000 * 60 * 15))); // 15 mins
     }
 };
 
