97af7052d3ef67f6879751cc35dc54f8186ad1c1
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-10-26 23:53:58 +0300

    add code to StateChangedResult

--- StateChangedResult.h
+++ StateChangedResult.h
@@ -1,14 +1,17 @@
 #ifndef StateChangedResult_h
 #define StateChangedResult_h
 
+#include "_uint8_t.h"
+
 struct StateChangedResult
 {
 public:
     bool isChanged;
     bool newState;
+    uint8_t newStateCode;
 
-    StateChangedResult(bool isChanged, bool newState)
-        : isChanged(isChanged), newState(newState)
+    StateChangedResult(bool isChanged, bool newState, uint8_t newStateCode = 0)
+        : isChanged(isChanged), newState(newState), newStateCode(newStateCode)
     {
     }
 };
