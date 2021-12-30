779b3aa402796085c28e3bce1f476a0e365cfa42
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2021-12-30 16:47:33 +0200

    define pin numbers so they can be interchangeable on arduino and esp

--- Definitions.h
+++ Definitions.h
@@ -6,4 +6,14 @@
 #define HANDLER_ARGS_PASS results, devices, devicesCount, controllers, controllersCount
 #define REAL_BOARD __AVR || ARDUINO_ESP8266_NODEMCU
 
+#define D0 #if _AVR 9 #elif ARDUINO_ESP8266_NODEMCU 0 #else - 1
+#define D1 #if _AVR 10 #elif ARDUINO_ESP8266_NODEMCU 1 #else - 1
+#define D2 2
+#define D3 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define D8 8
+
 #endif
