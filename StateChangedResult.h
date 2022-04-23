18dbbde7627b666b7ef63d05d7c02dcb26c98aa5
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2022-04-23 12:58:54 +0300

    add todo

--- StateChangedResult.h
+++ StateChangedResult.h
@@ -6,9 +6,16 @@
 struct StateChangedResult
 {
 public:
+    // TODO: remove these two in favour of newStateCode
     bool isChanged;
     bool newState;
+
     uint8_t newStateCode;
+
+    bool isClicked()
+    {
+        return newStateCode != 0;
+    }
 
     StateChangedResult(bool isChanged, bool newState, uint8_t newStateCode = 0)
         : isChanged(isChanged), newState(newState), newStateCode(newStateCode)
