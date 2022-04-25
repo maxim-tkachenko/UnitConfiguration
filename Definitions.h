f6b236e246ddc2230f46de4f7fe56d1b51aa22db
Maxim Tkachenko <makcim.tkachenko@gmail.com>
2022-04-25 17:18:29 +0300

    update named pins for avr/esp

--- Definitions.h
+++ Definitions.h
@@ -4,14 +4,29 @@
 #define WORKUNIT_ARGS IDevice **devices, uint8_t devicesCount, IController **controllers, uint8_t controllersCount
 #define HANDLER_ARGS bool *results, WORKUNIT_ARGS
 #define HANDLER_ARGS_PASS results, devices, devicesCount, controllers, controllersCount
-#define REAL_BOARD __AVR || ARDUINO_ESP8266_NODEMCU
 
 #define WIFI_NETWORK_CONNECTION
 // #define WIRE_NETWORK_CONNECTION
 
-#ifndef ARDUINO_ESP8266_NODEMCU
-#define DPIN0 9
-#define DPIN1 10
+#if defined(ARDUINO_ARCH_ESP8266)
+#define REAL_BOARD true
+// pinout for node mcu
+#define DPIN0 16 // ? x
+#define DPIN1 5
+#define DPIN2 4
+#define DPIN3 0
+#define DPIN4 2
+#define DPIN5 14
+#define DPIN6 12
+#define DPIN7 13
+#define DPIN8 15
+#define DPIN9 -1
+#define DPIN10 -1
+
+#elif defined(__AVR)
+#define REAL_BOARD true
+#define DPIN0 11
+#define DPIN1 12
 #define DPIN2 2
 #define DPIN3 3
 #define DPIN4 4
@@ -19,17 +34,11 @@
 #define DPIN6 6
 #define DPIN7 7
 #define DPIN8 8
-#endif
+#define DPIN9 9
+#define DPIN10 10
 
-// pinout for mode mcu
-// #define D0 16? x
-// #define D1 5
-// #define D2 4
-// #define D3 0
-// #define D4 2
-// #define D5 14
-// #define D6 12
-// #define D7 13
-// #define D8 15
+#else
+#define REAL_BOARD false
+#endif
 
 #endif
